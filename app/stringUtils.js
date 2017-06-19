4bf1e2c7618b8d299300c6ef0f1933ec1dbb9688
Dan Wellman <danwellman@hotmail.com>
2017-06-19 19:50:36 +0100

    repro

---
+++ app/stringUtils.js
@@ -0,0 +1,7 @@
+﻿export default class stringUtils {
+
+    static reverse(str) {
+        return str.split('').reverse().join('');
+    }
+
+}
---
+++ systemjs-project.js
@@ -0,0 +1,10 @@
+﻿import stringUtils from 'app/stringUtils.js';
+import numberUtils from 'app/amd/utils.js';
+import $ from 'jquery';
+import minicolors from 'claviska/jquery-minicolors';
+import 'minicolors.css';
+
+console.log(stringUtils.reverse('reverse me'));
+console.log(numberUtils.cube(10));
+
+$('#colorPicker').minicolors();
